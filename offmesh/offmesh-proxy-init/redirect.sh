aa37542566f1439a6a300a2082c54ab2bd016e37
he-jingkai-mbp <he-jingkai@outlook.com>
2022-10-13 15:15:59 +0800

    redirect

--- offmesh/offmesh-proxy-init/redirect.sh
+++ offmesh/offmesh-proxy-init/redirect.sh
@@ -1,16 +1,18 @@
 POD_IP=$1
-echo POD_IP="${POD_IP}"
+PROXY_IP=`tail -n 1 /etc/hosts | awk '{print $1}'`
+echo POD_IP=${POD_IP}
+echo PROXY_IP=${PROXY_IP}
+
 # inbound (dst = micro-svc pod's ip)
 iptables -t nat -N ISTIO_INBOUND
 iptables -t nat -A ISTIO_INBOUND -p tcp --dport 15020 -j REDIRECT --to 15020
 iptables -t nat -A ISTIO_INBOUND -p tcp --dport 15021 -j REDIRECT --to 15021
 iptables -t nat -A ISTIO_INBOUND -p tcp -j REDIRECT --to 15006
 
-iptables -t nat -A PREROUTING -p tcp -d "$POD_IP" -j ISTIO_INBOUND
+iptables -t nat -A PREROUTING -p tcp -d $POD_IP -j ISTIO_INBOUND
 
 # outbound (src = micro-svc pod's ip)
-
-iptables -t nat -A PREROUTING -s "$POD_IP" ! -d $PROXY_IP -p tcp -j REDIRECT --to 15001
+iptables -t nat -A PREROUTING -s $POD_IP ! -d $PROXY_IP -p tcp -j REDIRECT --to 15001
 
 echo "--- iptables ---"
 iptables -t nat -L -n --line-numbers
